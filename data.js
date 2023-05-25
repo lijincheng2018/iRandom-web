8a2fcc848bbebdb2c6c662d43f6b975d4c4d3809
LJC <lijincheng2016@qq.com>
2023-05-25 16:05:48 +0800

    upload v1
    v1

---
+++ data.js
@@ -0,0 +1,12 @@
+function getData() {
+    let data = [
+        {
+            name: '李锦成'
+        }, {
+            name: '测试者1'
+        }, {
+            name: '测试者2'
+        }
+    ];
+    return data;
+}
